abdbc6753543be03a7677c417898eabbc6be2acc
Mani <ram.narayan.mani@sap.com>
2017-04-17 00:26:55 +0800

    add

--- node-hello/app.js
+++ node-hello/app.js
@@ -1,10 +1,22 @@
 var express = require('express')
 var app = express()
+var fs = require("fs");
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
+
+app.get('/sample', function (req, res) {
+	fs.readFile("sample.html", function(err, data){
+	  res.writeHead(200, {'Content-Type': 'text/html'});
+	  res.write(data);
+	  res.end();
+	});
+})
+
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 })
+
